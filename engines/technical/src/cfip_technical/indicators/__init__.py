cb398c6d2eb5d8c5e8996119a251ba4b9c7e001e
Arman Emp <74330704+armanemp@users.noreply.github.com>
2026-09-16 02:40:34 +0330

    feat(technical): expose all canonical family implementations

--- engines/technical/src/cfip_technical/indicators/__init__.py
+++ engines/technical/src/cfip_technical/indicators/__init__.py
@@ -5,17 +5,9 @@ is part of the canonical technical namespace.
 """
 
 from ..models import IndicatorResult, OHLCV
-from .core import atr, bollinger_bands, ema, macd, rsi, sma
-from .oscillators import (
-    cci,
-    money_flow_index,
-    momentum,
-    roc,
-    stochastic,
-    stochastic_rsi,
-    williams_r,
-)
-from .trend import adx, aroon, donchian_channels, ichimoku, keltner_channels
+from .core import atr, bollinger_bands, dema, ema, macd, rsi, sma, tema
+from .oscillators import cci, money_flow_index, momentum, roc, stochastic, stochastic_rsi, trix, williams_r
+from .trend import adx, aroon, donchian_channels, ichimoku, keltner_channels, parabolic_sar
 from .volume import chaikin_money_flow, obv, vwap
 
 __all__ = [
@@ -27,6 +19,7 @@ __all__ = [
     "bollinger_bands",
     "cci",
     "chaikin_money_flow",
+    "dema",
     "donchian_channels",
     "ema",
     "ichimoku",
@@ -35,11 +28,14 @@ __all__ = [
     "money_flow_index",
     "momentum",
     "obv",
+    "parabolic_sar",
     "roc",
     "rsi",
     "sma",
     "stochastic",
     "stochastic_rsi",
+    "tema",
+    "trix",
     "vwap",
     "williams_r",
 ]
